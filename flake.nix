33851bc9cb607690c93eb50b8529348aef4d9a97
Herman Lederer <germans.lederers@gmail.com>
2023-02-23 16:33:28 +0100

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "pnpm with internal Node v18";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pnpm = pkgs.nodePackages.pnpm.override {
+          nodejs = pkgs.nodejs-18_x;
+        };
+      in {
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            pnpm
+          ];
+        };
+      }
+    );
+}
